c1f0f72a39c45cc2db43c1ac23ce7ed605c5ec97
Joseph Rymer <josephrymer983@gmail.com>
2017-01-18 20:44:00 -0600

    Minor Changes 01
    Added new Config Files and setup prepration for major changes 01

---
+++ Joseph983.TrackableLifts/TrackableLifts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+    //#region
+    //private string XMLConfigFile = "TrackableLiftsConfig.xml";
+    //private string XMLConfigPath = "";
+    //private string XMLModID = "Joseph983.TrackableLifts";
+    //private string int XMLModVersion = 1;
+    //private bool XMLConfigExists;
+    //public TrackableLiftsConfig mConfig;
+    //#endregion
+    public class TrackableLifts : FortressCraftMod
+    {
+        public override ModRegistrationData Register()
+        {
+            return base.Register();
+        }
+    }
+
---
+++ Joseph983.TrackableLifts/TrackableLiftsConfig.cs
@@ -0,0 +1,7 @@
+﻿public class TrackableLiftsConfig
+{
+ public int TLiftShaftBuilderPPS = 200;
+public int TLiftShaftBuilderPowerStorage = 600;
+public int GuideRailFormerPPS = 10;
+public int GuideRailFormerPowerStorage = 100;
+}
